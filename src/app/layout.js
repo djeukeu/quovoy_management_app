791413154c677846fa31e588cee21941a6d87ae4
djeukeu <dmxchristian@gmail.com>
2025-09-20 19:12:49 +0100

    init

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Lead Management App",
+  description: "Quovoy Coding Test",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
